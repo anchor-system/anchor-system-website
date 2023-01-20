801c82defd9273ed240f496ecbea1030dca771df
ccn <ccn@cuppajoeman.com>
2023-01-20 13:30:54 -0500

    collections depend on days

--- applications/random_collection/random_collection.js
+++ applications/random_collection/random_collection.js
@@ -43,22 +43,8 @@ let tone_collections = [
     [0, 1, 3, 4, 6, 7, 9]
 ]
 
-function generateRandomToneCollection(size=7) {
-    let bucket = [];
-    for (let i=1;i<=11;i++) {
-        bucket.push(i);
-    }
-    function getRandomFromBucket() {
-        let randomIndex = Math.floor(Math.random()*bucket.length);
-        return bucket.splice(randomIndex, 1)[0];
-    }
-    let toneCollection = [0]; // Always contains 0
-    for (let i = 0; i < size - 1; i ++){
-        toneCollection.push(getRandomFromBucket())
-    }
-    toneCollection.sort(function(a,b){return a - b});
-    return toneCollection;
-}
 
 // document.getElementById("tone_collection").innerHTML = generateRandomToneCollection().join(" ");
-document.getElementById("tone_collection").innerHTML = tone_collections[Math.floor(Math.random() * tone_collections.length)].join(' ');
+var now = new Date();
+var fullDaysSinceEpoch = Math.floor(now/8.64e7);
+document.getElementById("tone_collection").innerHTML = tone_collections[fullDaysSinceEpoch % tone_collections.length].join(' ');
