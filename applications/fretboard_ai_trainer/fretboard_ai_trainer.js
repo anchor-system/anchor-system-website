8b33657b9ebab284b8b3cb55074d9dcd690e8c43
ccn <ccn@cuppajoeman.com>
2023-01-24 10:58:48 -0500

    add more stats

--- applications/fretboard_ai_trainer/fretboard_ai_trainer.js
+++ applications/fretboard_ai_trainer/fretboard_ai_trainer.js
@@ -1,9 +1,13 @@
 let fretLine = document.getElementById("fret_line");
 let locateText = document.getElementById("locate_text");
+let numFoundText = document.getElementById("num_found");
+let numSolvedText = document.getElementById("num_solved");
 
 let stringAnchorInterval = -1
 let locateAnchorInterval = -1
 let chosenString = -1
+let positionsFound = new Set()
+let numQuestionsSolved = 0
 
 let keyMap =
     [
@@ -44,7 +48,7 @@ function isCorrect() {
     return numFound >= 2;
 }
 
-function guessedPositionToAnchorInterval(x, y) {
+function positionToAnchorInterval(x, y) {
     let stringOffset = x - chosenString
     let fretOffset = y - 1
     let anchorIntervalChange = stringOffset * 5 + fretOffset
@@ -52,12 +56,20 @@ function guessedPositionToAnchorInterval(x, y) {
     return posMod(guessedAnchorInterval, 12)
 }
 
+function numPossible(origin, originAnchorInterval, locateAnchorInterval) {
+    let x, y = origin
+
+}
+
 function generateSituation() {
+    positionsFound = new Set()
     chosenString = Math.floor(Math.random() * 6);
     stringAnchorInterval = Math.floor(Math.random() * 12);
     locateAnchorInterval = Math.floor(Math.random() * 12);
 
     locateText.innerHTML = "LOCATE: " + locateAnchorInterval.toString()
+    numFoundText.innerHTML = "NUM FOUND: " + positionsFound.size + "/" + numPositionsPossible(locateAnchorInterval)
+    numSolvedText.innerHTML = "SOLVED: " + numQuestionsSolved
 
 
     for (let i = 0; i < 6; i ++) {
@@ -72,17 +84,35 @@ function generateSituation() {
     }
 }
 
-document.body.onkeydown = function(e){
+function numPositionsPossible(locateAnchorInterval) {
+    let numPossible = 0
+    for (let y = 0; y < keyMap.length; y ++) {
+        for (let x = 0; x < keyMap[0].length; x++) {
+            if (positionToAnchorInterval(x, y) === locateAnchorInterval) {
+                numPossible += 1
+            }
+        }
+    }
+    return numPossible;
+}
 
+document.body.onkeydown = function(e){
     for (let y = 0; y < keyMap.length; y ++) {
         for (let x = 0; x < keyMap[0].length; x++) {
             let key = keyMap[y][x]
             if (e.key === key) {
-                let correct = guessedPositionToAnchorInterval(x, y) === locateAnchorInterval
+                let correct = positionToAnchorInterval(x, y) === locateAnchorInterval
                 if (correct) {
-                    generateSituation()
+                    positionsFound.add(x.toString(10) + y.toString(10))
+                    let enough = positionsFound.size === numPositionsPossible(locateAnchorInterval)
+                    if (enough) {
+                        numQuestionsSolved += 1
+                        generateSituation()
+                    }
                 }
+
             }
+            numFoundText.innerHTML = "NUM FOUND: " + positionsFound.size + "/" + numPositionsPossible(locateAnchorInterval)
         }
     }
 }
