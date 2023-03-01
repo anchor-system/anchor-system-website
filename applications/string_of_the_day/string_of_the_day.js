533132fe2843ef1387999e1ef82836e63b0de37a
ccn <ccn@cuppajoeman.com>
2023-03-01 15:31:07 -0500

    fix string of the day

--- applications/string_of_the_day/string_of_the_day.js
+++ applications/string_of_the_day/string_of_the_day.js
@@ -1,4 +1,5 @@
 // document.getElementById("tone_collection").innerHTML = generateRandomToneCollection().join(" ");
 var now = new Date();
+var numStrings = 6
 var fullDaysSinceEpoch = Math.floor(now/8.64e7);
-document.getElementById("string_of_the_day").innerHTML = (fullDaysSinceEpoch % 7) + 1
+document.getElementById("string_of_the_day").innerHTML = (fullDaysSinceEpoch % numStrings) + 1
